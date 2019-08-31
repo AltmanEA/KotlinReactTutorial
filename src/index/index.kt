e44142a2f43f606926c7ea8d2283e52af59b5abe
AltmanEA <altmanea@gmail.com>
2019-08-31 20:46:58 +0600

    Hello world

---
+++ src/index/index.kt
@@ -0,0 +1,13 @@
+package index
+
+import kotlinext.js.*
+import react.dom.*
+import kotlin.browser.*
+
+fun main(args: Array<String>) {
+    requireAll(require.context("src", true, js("/\\.css$/")))
+
+    render(document.getElementById("root")) {
+        p{+"Hello world"}
+    }
+}
