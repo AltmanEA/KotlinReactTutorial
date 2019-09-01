7d664084b8d002f893a55b72ac934943c3a7d52e
AltmanEA <altmanea@gmail.com>
2019-09-01 12:40:17 +0600

    Taking turns

--- src/game/Board.kt
+++ src/game/Board.kt
@@ -7,26 +7,29 @@ class Board: RComponent<RProps, Board.State>() {
 
     init {
         state.squares = Array(9){' '}
+        state.xIsNext = true
     }
 
     interface State:RState{
         var squares: Array<Char>
+        var xIsNext: Boolean
     }
 
     fun handleClick(i: Int) {
         val tmp = state.squares.copyOf()
-        tmp[i]='X'
+        tmp[i] = if (state.xIsNext) 'X' else '0'
         setState{
             squares = tmp
+            xIsNext = !xIsNext
         }
     }
 
-    val status = "Next player: X"
     fun RBuilder.renderSquare(i: Int) {
         square(state.squares[i], {handleClick(i)})
     }
 
     override fun RBuilder.render() {
+        val status = "Next player: ${if (state.xIsNext) 'X' else '0'}"
         div(classes = "status"){ +status }
         div {
             div(classes = "board-row") {
